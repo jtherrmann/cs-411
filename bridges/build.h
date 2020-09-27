2198e106785776d1edc5e3e20060e39053fca9d6
Jake Herrmann <jtherrmann1@gmail.com>
2020-09-27 13:40:31 -0800

    Fail tests by returning -1

---
+++ bridges/build.h
@@ -0,0 +1,11 @@
+#ifndef BRIDGES_BUILD_H
+#define BRIDGES_BUILD_H
+
+#include <vector>
+using std::vector;
+
+using Bridge = vector<int>;
+
+int build(int w, int e, const vector<Bridge> & bridges);
+
+#endif //BRIDGES_BUILD_H
