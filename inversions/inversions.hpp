ab53cf4f0e29164f5dc82cb4a680823b38851621
Jake Herrmann <jtherrmann1@gmail.com>
2020-10-12 13:20:39 -0800

    Fail inversions tests

---
+++ inversions/inversions.hpp
@@ -0,0 +1,15 @@
+//
+// Created by jth on 10/12/20.
+//
+
+#ifndef INVERSIONS_INVERSIONS_HPP
+#define INVERSIONS_INVERSIONS_HPP
+
+#include <cstddef>
+
+template<typename RAIter>
+size_t inversions(RAIter first, RAIter last) {
+    return -1;
+}
+
+#endif //INVERSIONS_INVERSIONS_HPP
