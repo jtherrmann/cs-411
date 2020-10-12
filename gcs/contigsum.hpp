3fdde2e6e07d732d76f8b4f8b26c71218b8c415a
Jake Herrmann <jtherrmann1@gmail.com>
2020-10-11 17:37:53 -0800

    Fail GCS tests

---
+++ gcs/contigsum.hpp
@@ -0,0 +1,13 @@
+//
+// Created by jth on 10/11/20.
+//
+
+#ifndef GCS_CONTIGSUM_HPP
+#define GCS_CONTIGSUM_HPP
+
+template<typename RAIter>
+int contigSum(RAIter first, RAIter last) {
+    return -1;
+}
+
+#endif //GCS_CONTIGSUM_HPP
