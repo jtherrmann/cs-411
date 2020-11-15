09342e7e39232180b58bfbe4a95907003022a842
Jake Herrmann <jtherrmann1@gmail.com>
2020-11-14 18:18:41 -0900

    Pass bridges-2 tests

--- bridges-2/build.cpp
+++ bridges-2/build.cpp
@@ -1,7 +1,40 @@
-#include "build.hpp"
+// Jake Herrmann
+// CS 411
+// 14 Nov 2020
+//
+// build.cpp
+// Source file for the dynamic programming bridge-building algorithm.
 
+
+#include <algorithm>
+using std::max;
+
+#include "build.hpp"
 using std::vector;
 
+
 int build(int w, int e, const vector<Bridge> & bridges) {
-    return -1;
+    auto tolls = vector<vector<int>>(w + 1, vector<int>(e + 1, 0));
+
+    // Store the individual bridge tolls in the table.
+    for(const auto & bridge : bridges) {
+        auto row = bridge[0] + 1;
+        auto col = bridge[1] + 1;
+        auto toll = bridge[2];
+
+        // There may be multiple possible bridges between the same two cities.
+        tolls[row][col] = max(toll, tolls[row][col]);
+    }
+
+    // Fill up the table with the max cumulative tolls.
+    for(int row = 1; row <= w; ++row) {
+        for(int col = 1; col <= e; ++col) {
+            tolls[row][col] = max({
+                tolls[row - 1][col],
+                tolls[row][col - 1],
+                tolls[row - 1][col - 1] + tolls[row][col]
+            });
+        }
+    }
+    return tolls[w][e];
 }
--- bridges-2/build.hpp
+++ bridges-2/build.hpp
@@ -1,3 +1,10 @@
+// Jake Herrmann
+// CS 411
+// 14 Nov 2020
+//
+// build.hpp
+// Header file for the dynamic programming bridge-building algorithm.
+
 #ifndef BRIDGES_BUILD_H
 #define BRIDGES_BUILD_H
 
