ae9ef773b54247b70269f347851d6829169e75e2
Jake Herrmann <jtherrmann1@gmail.com>
2020-11-14 15:26:22 -0900

    Fail bridges-2 tests

---
+++ bridges-2/build.cpp
@@ -0,0 +1,7 @@
+#include "build.hpp"
+
+using std::vector;
+
+int build(int w, int e, const vector<Bridge> & bridges) {
+    return -1;
+}
---
+++ bridges-2/build.hpp
@@ -0,0 +1,10 @@
+#ifndef BRIDGES_BUILD_H
+#define BRIDGES_BUILD_H
+
+#include <vector>
+
+using Bridge = std::vector<int>;
+
+int build(int w, int e, const std::vector<Bridge> & bridges);
+
+#endif //BRIDGES_BUILD_H
